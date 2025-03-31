21519a37566a0ed17198dcb77b4c0f2daeda4374
Bellika <markusbellika@gmail.com>
2025-03-31 18:23:40 +0200

    changed app structure

--- lektion-7/doomkeep-be/app.py
+++ lektion-7/doomkeep-be/app.py
@@ -7,29 +7,34 @@ from routes.npc_riddle_routes import npc_riddle_routes
 from routes.user_routes import user_routes
 from routes.auth_routes import auth_routes
 
-app = Flask(__name__)
-CORS(app, supports_credentials=True)
-app.secret_key = 'secret-key'
+def create_app():
+  app = Flask(__name__)
+  CORS(app, supports_credentials=True)
+  app.secret_key = 'secret-key'
+  
+  app.config['SQLALCHEMY_DATABASE_URI'] = get_database_uri()
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  
+  db.init_app(app)
+  
+  app.register_blueprint(auth_routes, url_prefix='/api/auth')
+  app.register_blueprint(user_routes, url_prefix='/api/user')
+  app.register_blueprint(character_routes,  url_prefix='/api')
+  app.register_blueprint(enemy_routes, url_prefix='/api')
+  app.register_blueprint(npc_riddle_routes, url_prefix='/api')
+ 
+  @app.route('/view-story', methods=['GET'])
+  def view_story():
+      story = session.get('story', '')
+      return jsonify({"story": story})
+  
+  @app.route('/ping')
+  def ping():
+    return 'Server is running'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = get_database_uri()
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  return app
 
-db.init_app(app)
-
-app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(user_routes, url_prefix='/api/user')
-app.register_blueprint(character_routes,  url_prefix='/api')
-app.register_blueprint(enemy_routes, url_prefix='/api')
-app.register_blueprint(npc_riddle_routes, url_prefix='/api')
-
-@app.route('/view-story', methods=['GET'])
-def view_story():
-    story = session.get('story', '')
-    return jsonify({"story": story})
-
-@app.route('/ping')
-def ping():
-  return 'Server is running'
 
 if __name__ == '__main__':
+  app = create_app()
   app.run(host='0.0.0.0', port=5000, debug=True)
